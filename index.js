3270ec523793a1ffc4d98e5cd58bd1dde219f427
Andres Parra <byandrev@gmail.com>
2026-05-08 09:08:58 -0500

    fix: update CORS origin handling to support multiple origins from environment variable

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.ORIGINS || "*",
+    origin: process.env.ORIGINS ? process.env.ORIGINS.split(",") : ["*"],
   }),
 );
 
